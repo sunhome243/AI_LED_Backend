ee17b52a489e43bdb04bdb0d59d17c9f0a7038bc
Sunho Kim <107231365+sunhome243@users.noreply.github.com>
2025-03-13 13:35:20 -0400

    Update Terraform workflow and add requirements files for Lambda functions

--- modules/compute/archive.tf
+++ modules/compute/archive.tf
@@ -6,24 +6,24 @@ locals {
 # Create deployment packages for Lambda functions
 data "archive_file" "audio_to_ai_lambda" {
   type        = "zip"
-  source_file = "${local.base_dir}/lambda/audio_to_ai/audio_to_ai.py"
+  source_dir  = "${local.base_dir}/lambda/audio_to_ai"
   output_path = "${path.module}/archive/audio_to_ai.zip"
 }
 
 data "archive_file" "pattern_to_ai_lambda" {
   type        = "zip"
-  source_file = "${local.base_dir}/lambda/pattern_to_ai/pattern_to_ai.py"
+  source_dir = "${local.base_dir}/lambda/pattern_to_ai"
   output_path = "${path.module}/archive/pattern_to_ai.zip"
 }
 
 data "archive_file" "result_save_send_lambda" {
   type        = "zip"
-  source_file = "${local.base_dir}/lambda/result_save_send/result_save_send.py"
+  source_dir = "${local.base_dir}/lambda/result_save_send"
   output_path = "${path.module}/archive/result_save_send.zip"
 }
 
 data "archive_file" "ws_messenger_zip" {
   type        = "zip"
-  source_file = "${local.base_dir}/lambda/websocket/connection_manager.py"
+  source_dir = "${local.base_dir}/lambda/websocket"
   output_path = "${path.module}/archive/ws_messenger.zip"
 }
