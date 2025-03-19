bfb3554233eb3d41cfd4623546282a2d4886c8d8
Sunho Kim <107231365+sunhome243@users.noreply.github.com>
2025-03-19 14:09:58 -0400

    Refactor IAM policies to reference existing policies instead of creating new ones for Lambda functions

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -16,114 +16,29 @@ resource "aws_iam_role" "iam_for_lambda" {
   description        = "IAM role for Lambda functions with permissions for DynamoDB, S3, CloudWatch, and API Gateway"
 }
 
-# CloudWatch Logs permissions
-resource "aws_iam_policy" "lambda_logging_policy" {
-  name        = "lambda_logging_policy"
-  description = "Allows Lambda functions to write logs to CloudWatch"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
+# Reference existing CloudWatch Logs permissions policy instead of creating it
+data "aws_iam_policy" "lambda_logging_policy" {
+  name = "lambda_logging_policy"
 }
 
-# API Gateway Management permissions
-resource "aws_iam_policy" "apigateway_management_policy" {
-  name        = "lambda_apigateway_management_policy"
-  description = "Allows Lambda functions to manage API Gateway WebSocket connections"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "execute-api:ManageConnections",
-          "execute-api:Invoke"
-        ]
-        Effect   = "Allow"
-        Resource = "*" # This will be attached later to specific API Gateway
-      }
-    ]
-  })
+# Reference existing API Gateway Management policy
+data "aws_iam_policy" "apigateway_management_policy" {
+  name = "lambda_apigateway_management_policy"
 }
 
-# DynamoDB access permissions
-resource "aws_iam_policy" "lambda_dynamodb_policy" {
-  name        = "lambda_dynamodb_policy"
-  description = "Allows Lambda functions to perform CRUD operations on DynamoDB tables"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:Query",
-          "dynamodb:Scan"
-        ]
-        Effect   = "Allow"
-        Resource = [
-          var.auth_table_arn,
-          var.ircode_table_arn,
-          var.response_table_arn,
-          var.connection_table_arn
-        ]
-      }
-    ]
-  })
+# Reference existing DynamoDB access policy
+data "aws_iam_policy" "lambda_dynamodb_policy" {
+  name = "lambda_dynamodb_policy"
 }
 
-# S3 access permissions
-resource "aws_iam_policy" "lambda_s3_policy" {
-  name        = "lambda_s3_policy"
-  description = "Allows Lambda functions to read from and write to S3 bucket"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:PutObject",
-          "s3:GetObject"
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:s3:::${var.response_bucket_name}/*"
-      }
-    ]
-  })
+# Reference existing S3 access policy
+data "aws_iam_policy" "lambda_s3_policy" {
+  name = "lambda_s3_policy"
 }
 
-# Lambda invocation policy for API Gateway
-resource "aws_iam_policy" "lambda_invoke_policy" {
-  name        = "lambda_invoke_policy"
-  description = "Allows API Gateway to invoke Lambda functions"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "lambda:InvokeFunction",
-          "lambda:GetFunction"
-        ]
-        Effect   = "Allow"
-        Resource = "*"  # You can restrict this to specific Lambda ARNs for better security
-      }
-    ]
-  })
+# Reference existing Lambda invocation policy
+data "aws_iam_policy" "lambda_invoke_policy" {
+  name = "lambda_invoke_policy"
 }
 
 resource "aws_iam_role" "api_gateway_role" {
@@ -143,91 +58,56 @@ resource "aws_iam_role" "api_gateway_role" {
   })
 }
 
-# API Gateway CloudWatch logging policy
-resource "aws_iam_policy" "api_gateway_logging_policy" {
-  name        = "api_gateway_logging_policy"
-  description = "Allows API Gateway to push logs to CloudWatch"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:PutLogEvents",
-          "logs:GetLogEvents",
-          "logs:FilterLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
+# Reference existing API Gateway CloudWatch logging policy
+data "aws_iam_policy" "api_gateway_logging_policy" {
+  name = "api_gateway_logging_policy"
 }
 
-# Lambda-to-Lambda invocation policy
-resource "aws_iam_policy" "lambda_to_lambda_policy" {
-  name        = "lambda_to_lambda_policy"
-  description = "Allows Lambda functions to invoke other Lambda functions"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "lambda:InvokeFunction",
-          "lambda:InvokeAsync"
-        ]
-        Effect   = "Allow"
-        Resource = "arn:aws:lambda:*:*:function:*"  # Consider restricting to specific functions for better security
-      }
-    ]
-  })
+# Reference existing Lambda-to-Lambda invocation policy
+data "aws_iam_policy" "lambda_to_lambda_policy" {
+  name = "lambda_to_lambda_policy"
 }
 
 # Attach all policies to Lambda role
 resource "aws_iam_role_policy_attachment" "lambda_apigateway_attachment" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.apigateway_management_policy.arn
+  policy_arn = data.aws_iam_policy.apigateway_management_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attachment" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+  policy_arn = data.aws_iam_policy.lambda_dynamodb_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_s3_attachment" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_s3_policy.arn
+  policy_arn = data.aws_iam_policy.lambda_s3_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs_attachment" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_logging_policy.arn
+  policy_arn = data.aws_iam_policy.lambda_logging_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "api_gateway_logs_attachment" {
   role       = aws_iam_role.api_gateway_role.name
-  policy_arn = aws_iam_policy.lambda_logging_policy.arn
+  policy_arn = data.aws_iam_policy.lambda_logging_policy.arn
 }
 
 # Attach API Gateway specific logging policy
 resource "aws_iam_role_policy_attachment" "api_gateway_cloudwatch_attachment" {
   role       = aws_iam_role.api_gateway_role.name
-  policy_arn = aws_iam_policy.api_gateway_logging_policy.arn
+  policy_arn = data.aws_iam_policy.api_gateway_logging_policy.arn
 }
 
 # Attach Lambda invoke permissions to API Gateway role
 resource "aws_iam_role_policy_attachment" "api_gateway_lambda_invoke_attachment" {
   role       = aws_iam_role.api_gateway_role.name
-  policy_arn = aws_iam_policy.lambda_invoke_policy.arn
+  policy_arn = data.aws_iam_policy.lambda_invoke_policy.arn
 }
 
 # Attach Lambda-to-Lambda invocation policy to Lambda role
 resource "aws_iam_role_policy_attachment" "lambda_to_lambda_attachment" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_to_lambda_policy.arn
+  policy_arn = data.aws_iam_policy.lambda_to_lambda_policy.arn
 }
